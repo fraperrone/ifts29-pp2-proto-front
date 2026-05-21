342c45bbe5b41a7ae474c29a69d651c0a2f470d9
Franco Perrone <fperronerey@gmail.com>
2026-05-20 22:40:29 -0300

    boton volver

--- src/components/buttons/BotonCancelar.jsx
+++ src/components/buttons/BotonCancelar.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const BotonCancelar = ( {message}) => {
+const BotonCancelar = ( {message="Volver"}) => {
   const navigate = useNavigate()
   const irAlMenuPrincipal = () => {
     navigate('/estudios')
